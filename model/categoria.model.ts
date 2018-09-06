801a498d96e5963d3c263bd396a5eb4d68b74445
MarcoPis <assacrosspismarc@gmail.com>
2018-09-06 15:38:05 +0200

    client

--- model/categoria.model.ts
+++
@@ -1,9 +0,0 @@
-export class Categoria {
-    id: number;
-    nome: string; 
-    segno:string;   
-}
-
-export const SEGNO_ENTRATA = "ENTRATA";
-export const SEGNO_USCITA = "USCITA";
-export const SEGNO = [SEGNO_ENTRATA, SEGNO_USCITA];
--- model/famiglia.model.ts
+++
@@ -1,4 +0,0 @@
-export class Famiglia {
-    id: number;
-    nome: string;    
-}
